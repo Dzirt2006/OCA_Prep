d23c569ce09b078577938315380c6f523eb547dd
Dzirt2006 <Dzirt-2006@mail.ru>
2020-09-29 17:10:20 -0400

    exceptions handling

---
+++ src/ExeptionsHandling/Propagate.java
@@ -0,0 +1,27 @@
+package ExeptionsHandling;
+
+import java.lang.*;
+
+public class Propagate {
+    public static void main(String[] args) {
+        Propagate prop=new Propagate();
+        try {
+            prop.reversed("");
+        }catch(Exception e){
+            System.out.println(e);
+        }finally {
+            System.out.println("Hey Ho!");
+        }
+    }
+
+    String reversed(String str) throws Exception {
+        if (str.length() < 1) {
+            throw new Exception("Auchtung! ");
+        }
+        String reversed = "";
+        for(int i=str.length()-1;i>=0;--i){
+            reversed+=str.charAt(i);
+        }
+        return reversed;
+    }
+}
